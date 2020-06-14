98698ddbc946cd14924e688b230a8fc82082862d
phamchien98 <phamchien22598hn@gmail.com>
2020-06-14 13:31:58 +0000

    xuli

---
+++ xuli/arrayTolist.js
@@ -0,0 +1,16 @@
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li>{number * 2}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
